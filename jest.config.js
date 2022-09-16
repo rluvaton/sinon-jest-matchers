ae70ec412007e574453655cdf9423a9fc4b6ab4d
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2022-09-16 12:07:08 +0300

    test(snapshot): remove ansi from snapshots

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
   testEnvironment: 'node',
   verbose: true,
   testMatch: ['<rootDir>/test/**/*.test.ts'],
+  // This is required as we test and generate our snapshots in Mac
+  // we need to convert ansi (escape codes) from the output for it to work in linux too
+  snapshotSerializers: ['jest-snapshot-serializer-ansi'],
   restoreMocks: true,
 };
